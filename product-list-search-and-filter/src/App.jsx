673a78ff480302b4f3c20c1d00101d124521800a
alokgoldy <alokkrgoldy@gmail.com>
2026-02-06 14:33:22 +0530

    feat: added sorting

--- product-list-search-and-filter/src/App.jsx
+++ product-list-search-and-filter/src/App.jsx
@@ -5,10 +5,16 @@ import ProductList from './components/ProductList';
 function App() {
   const [search, setSearch] = useState('');
   const [category, setCategory] = useState('ALL');
+  const [sort, setSort] = useState('NONE');
+
 
   const filteredProducts = (products || []).filter((product) =>
     product?.name?.toLowerCase()?.includes(search?.toLowerCase()))
     .filter((product) => category === 'ALL' ? true : product.category === category)
+
+  const sortedProducts =
+    sort === 'PRICE_LOW_HIGH' ? [...filteredProducts].sort((a, b) => a.price - b.price)
+      : filteredProducts;
 
   return (
     <div className='app'>
@@ -27,7 +33,15 @@ function App() {
         <option value='Mobile'>Mobile</option>
         <option value='Laptop'>Laptop</option>
       </select>
-      <ProductList products={filteredProducts} />
+
+      <select
+        value={sort}
+        onChange={(e) => setSort(e.target.value)}
+      >
+        <option value={'NONE'}> No Sort</option>
+        <option value={'PRICE_LOW_HIGH'}>Price: Low - High</option>
+      </select>
+      <ProductList products={sortedProducts} />
     </div>
   )
 }
