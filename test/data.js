71aa73e119434f0f41f3336c0cf9b106da89a185
alokgoldy <alokkrgoldy@gmail.com>
2026-02-14 20:47:58 +0530

    feat: added data

---
+++ test/data.js
@@ -0,0 +1,57 @@
+export const quizData = [
+    {
+        id: 1,
+        question: "What does HTML stand for?",
+        options: [
+            "Hyper Text Markup Language",
+            "High Tech Modern Language",
+            "Hyper Transfer Markup Language",
+            "Home Tool Markup Language"
+        ],
+        correctAnswer: 0
+    },
+    {
+        id: 2,
+        question: "Which CSS property is used to change the text color of an element?",
+        options: [
+            "text-style",
+            "font-color",
+            "color",
+            "text-color"
+        ],
+        correctAnswer: 2
+    },
+    {
+        id: 3,
+        question: "What is the output of `typeof null` in JavaScript?",
+        options: [
+            "'null'",
+            "'undefined'",
+            "'object'",
+            "'number'"
+        ],
+        correctAnswer: 2
+    },
+    {
+        id: 4,
+        question: "Which method is used to remove the last element from an array in JavaScript?",
+        options: [
+            "shift()",
+            "pop()",
+            "push()",
+            "slice()"
+        ],
+        correctAnswer: 1
+    },
+    {
+        id: 5,
+        question: "What is the purpose of the `z-index` property in CSS?",
+        options: [
+            "To set the opacity of an element",
+            "To control the vertical stacking order of elements",
+            "To zoom into an element",
+            "To set the horizontal position of an element"
+        ],
+        correctAnswer: 1
+    }
+];
