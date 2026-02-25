b277e2027e95514c88f968e434055bb87ef312a7
alokgoldy <alokkrgoldy@gmail.com>
2026-02-25 19:43:25 +0530

    fix: fixed formatting issue

--- product-list-search-and-filter/src/App.jsx
+++ product-list-search-and-filter/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { products } from './data';
 import ProductList from './components/ProductList';
-import useDebounce from "./hooks/useDebounce";
+import useDebounce from './hooks/useDebounce';
 
 function App() {
   const [search, setSearch] = useState('');
@@ -17,42 +17,41 @@ function App() {
   //   return () => clearTimeout(timer);
   // }, [search])
 
-  const filteredProducts = (products || []).filter((product) =>
-    product?.name?.toLowerCase()?.includes(debouncedSearch?.toLowerCase()))
-    .filter((product) => category === 'ALL' ? true : product.category === category)
+  const filteredProducts = (products || [])
+    .filter((product) =>
+      product?.name?.toLowerCase()?.includes(debouncedSearch?.toLowerCase())
+    )
+    .filter((product) =>
+      category === 'ALL' ? true : product.category === category
+    );
 
   const sortedProducts =
-    sort === 'PRICE_LOW_HIGH' ? [...filteredProducts].sort((a, b) => a.price - b.price)
+    sort === 'PRICE_LOW_HIGH'
+      ? [...filteredProducts].sort((a, b) => a.price - b.price)
       : filteredProducts;
 
   return (
-    <div className='app'>
+    <div className="app">
       <h1>Product List</h1>
       <input
-        type='text'
-        placeholder='Search Products...'
+        type="text"
+        placeholder="Search Products..."
         value={search}
         onChange={(e) => setSearch(e.target.value)}
       />
-      <select
-        value={category}
-        onChange={e => setCategory(e.target.value)}
-      >
-        <option value='ALL'>ALL</option>
-        <option value='Mobile'>Mobile</option>
-        <option value='Laptop'>Laptop</option>
+      <select value={category} onChange={(e) => setCategory(e.target.value)}>
+        <option value="ALL">ALL</option>
+        <option value="Mobile">Mobile</option>
+        <option value="Laptop">Laptop</option>
       </select>
 
-      <select
-        value={sort}
-        onChange={(e) => setSort(e.target.value)}
-      >
+      <select value={sort} onChange={(e) => setSort(e.target.value)}>
         <option value={'NONE'}> No Sort</option>
         <option value={'PRICE_LOW_HIGH'}>Price: Low - High</option>
       </select>
       <ProductList products={sortedProducts} />
     </div>
-  )
+  );
 }
 
 export default App;
