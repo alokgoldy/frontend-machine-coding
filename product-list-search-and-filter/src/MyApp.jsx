e91b28c19a36563e63a478be5df370bb0ba25e3c
alokgoldy <alokkrgoldy@gmail.com>
2026-02-25 20:39:51 +0530

    feat: added naive logic

--- product-list-search-and-filter/src/MyApp.jsx
+++ product-list-search-and-filter/src/MyApp.jsx
@@ -1,13 +1,51 @@
+import { useState } from 'react';
+import MyProductList from './myComponents/MyProductList';
 import { myProducts } from './myData';
 
 function MyApp() {
+  const [search, setSearch] = useState('');
+  const [category, setCategory] = useState('ALL');
+  const [sort, setSort] = useState('NONE');
+  const sortedProducts = [...myProducts];
+
+  let finalProducts =
+    category === 'ALL'
+      ? [...sortedProducts]
+      : [...sortedProducts.filter((item) => item.category === category)];
+
+  finalProducts =
+    search !== ''
+      ? finalProducts.filter((item) =>
+          item.name.toLocaleLowerCase().includes(search?.toLocaleLowerCase())
+        )
+      : finalProducts;
+
+  if (sort !== 'NONE') {
+    finalProducts = [...finalProducts].sort((a, b) =>
+      sort === 'LOW_HIGH' ? a.price - b.price : b.price - a.price
+    );
+  }
+
   return (
-    <div className="app">
-      <ul>
-        {myProducts.map((item) => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ul>
+    <div className="my-app">
+      <h3>Product List</h3>
+      <input
+        type="text"
+        value={search}
+        placeholder={'Search Products'}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <select value={category} onChange={(e) => setCategory(e.target.value)}>
+        <option value="ALL">ALL</option>
+        <option value="Mobile">Mobile</option>
+        <option value="Laptop">Laptop</option>
+      </select>
+      <select value={sort} onChange={(e) => setSort(e.target.value)}>
+        <option value="NONE">NONE</option>
+        <option value="HIGH_LOW">HIGH-LOW</option>
+        <option value="LOW_HIGH">LOW-HIGH</option>
+      </select>
+      <MyProductList myProducts={finalProducts} />
     </div>
   );
 }
--- product-list-search-and-filter/src/myComponents/MyProductList.jsx
+++ product-list-search-and-filter/src/myComponents/MyProductList.jsx
@@ -2,6 +2,16 @@ import MyProductCard from './MyProductCard';
 
 function MyProductList({ myProducts }) {
   if (!myProducts?.length) {
-    return <p>No products..</p>;
+    return <p>No Products Found</p>;
   }
+
+  return (
+    <div className="my-product-list">
+      {myProducts.map((myProduct) => {
+        return <MyProductCard key={myProduct.id} myProduct={myProduct} />;
+      })}
+    </div>
+  );
 }
+
+export default MyProductList;
