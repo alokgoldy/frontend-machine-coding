a71563d501c4cb47975906a69c865f42f907a3d7
alokgoldy <alokkrgoldy@gmail.com>
2026-02-25 19:43:53 +0530

    fix: fixed formatting

--- product-list-search-and-filter/src/components/ProductCard.jsx
+++ product-list-search-and-filter/src/components/ProductCard.jsx
@@ -1,12 +1,13 @@
-
 function ProductCard({ product }) {
-    const { name, category, price } = product || {};
+  const { name, category, price } = product || {};
 
-    return (<div className="product-card">
-        <h3>{name}</h3>
-        <p>Category: {category}</p>
-        <p>Price: {price}</p>
-    </div>)
+  return (
+    <div className="product-card">
+      <h3>{name}</h3>
+      <p>Category: {category}</p>
+      <p>Price: {price}</p>
+    </div>
+  );
 }
 
 export default ProductCard;
