701e4fb561156ebaef0c9bcba93e47e8c779ec70
alokgoldy <alokkrgoldy@gmail.com>
2026-03-09 21:13:50 +0530

    feat: added debounced file

---
+++ react-test/src/pages/DebouncedSearch.jsx
@@ -0,0 +1,71 @@
+import { useState, useEffect } from 'react';
+
+function DebouncedSearch() {
+    const [search,setSearch] = useState('');
+    const [users, setUsers] = useState([]);
+    const [suggetions, setSuggestions] = useState([]);
+    const [selectedUser, setSelectedUser] = useState(null);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => setUsers(data));
+    },[])
+
+    useEffect(() =>{
+        const timerId = setTimeout(()=> {
+            if(search){
+                const filtered = users.filter((user) => {
+                return user.name.toLowerCase().includes(search.toLowerCase());
+                })
+                setSuggestions(filtered);
+            } else {
+                setSuggestions([]);
+            }
+        }, 500);
+        return () => clearTimeout(timerId);
+    }, [search, users])
+
+    const handleSelect = (user) => {
+        setSelectedUser(user);
+        setSearch(user.name);
+        setSuggestions([]);
+    }
+
+    return(
+        <div className='d-container'>
+            <div className='d-search-bar'>
+                <input
+                    placeholder='Search users ...'
+                    onChange={(e)=> setSearch(e.target.value)}
+                    type='text'
+                    className='d-search-input'
+                    value={search}
+                />
+                {suggetions.length > 0 && (
+                    <div className='d-suggestions'>
+                        {suggetions.map((user) =>(
+                            <div
+                            key={user.id}
+                            className='d-suggestions-user'
+                            onClick={() => handleSelect(user)}
+                            >
+                                {user.name}
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
+            {selectedUser && (
+                <div style={{ marginTop: "20px" }}>
+                    <h3>{selectedUser.name}</h3>
+                    <p>Email: {selectedUser.email}</p>
+                    <p>Phone: {selectedUser.phone}</p>
+                    <p>Website: {selectedUser.website}</p>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default DebouncedSearch;
