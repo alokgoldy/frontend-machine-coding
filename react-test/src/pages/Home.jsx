52a2fd5e8cad8d559fee4db31fb7f18bac639ff6
alokgoldy <alokkrgoldy@gmail.com>
2026-03-09 21:14:23 +0530

    feat: added home page

---
+++ react-test/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+function Home() {
+  return <div>
+    <h1>Home Page</h1>
+    <Link to="/debouncedsearch">Debounced Search</Link>
+    </div>;
+}
+
+export default Home;
