4ac49880203183c37a9a5909f843ccd371faf848
alokgoldy <alokkrgoldy@gmail.com>
2026-03-10 23:26:53 +0530

    update: revision

--- autocomplete-search/script.js
+++ autocomplete-search/script.js
@@ -2,19 +2,40 @@ const input = document.getElementById('searchInput');
 const suggestionsBox = document.getElementById('suggestions');
 
 let suggestions = [];
-let activeIndex = -1;
+// let activeIndex = -1;
 
 
 function debounce(fn, delay){
     let timer;
 
     return function(...args){
+        console.log('inpect@1', {
+            timer, args
+        })
         clearTimeout(timer);
 
         timer = setTimeout(()=> {
             fn.apply(this,args);
         },delay)
     }
+}
+
+
+function renderSuggestions(){
+    suggestionsBox.innerHTML = '';
+
+    suggestions.forEach((user, index) => {
+        const div = document.createElement('div');
+        div.classList.add('suggestion-item');
+        div.innerText = user.name;
+
+        div.addEventListener('click', ()=>{
+            input.value = user.name;
+            suggestionsBox.innerHTML = '';
+        })
+
+        suggestionsBox.appendChild(div);
+    })
 }
 
 async function fetchUsers(query){
@@ -31,3 +52,27 @@ async function fetchUsers(query){
 
     renderSuggestions();
 }
+
+// const debouncedSearch = debounce(fetchUsers, 500);
+
+// input.addEventListener('input', (e)=>{
+// debouncedSearch(e.target.value);
+// })
+
+function myDebounce(fn, delay){
+    let mytimer;
+
+    return function(...args){
+        clearTimeout(mytimer);
+        mytimer = setTimeout(()=>{
+            fn.apply(this,args);
+        },delay);
+    }
+}
+
+const myDebouncedSearch = myDebounce(fetchUsers, 500);
+
+
+input.addEventListener('input', (e)=>{
+    myDebouncedSearch(e.target.value);
+})
