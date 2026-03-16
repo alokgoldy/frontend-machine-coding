3f2b0047a340561dcbbff4968fd15a7bb675de7a
alokgoldy <alokkrgoldy@gmail.com>
2026-03-16 22:59:00 +0530

    feat: added app

--- react-test/src/App.jsx
+++ react-test/src/App.jsx
@@ -1,17 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from "./pages/Home";
+import Home from './pages/Home';
 import DebouncedSearch from './pages/DebouncedSearch';
 import AutoComplete from './pages/AutoComplete';
+import TodoApp from './pages/TodoApp';
 
 function App() {
-
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/debouncedsearch" element={<DebouncedSearch />} />
-      <Route path='/autocomplete' element={<AutoComplete/>}/>
+      <Route path="/autocomplete" element={<AutoComplete />} />
+      <Route path="/todo-app" element={<TodoApp />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
