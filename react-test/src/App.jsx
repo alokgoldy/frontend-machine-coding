9c664852cff78b3cec062d40b2d94cff8439778a
alokgoldy <alokkrgoldy@gmail.com>
2026-03-17 20:58:33 +0530

    update: updated the app file

--- react-test/src/App.jsx
+++ react-test/src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import DebouncedSearch from './pages/DebouncedSearch';
 import AutoComplete from './pages/AutoComplete';
 import TodoApp from './pages/TodoApp';
+import ImageCrousel from './pages/ImageCrousel';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/debouncedsearch" element={<DebouncedSearch />} />
       <Route path="/autocomplete" element={<AutoComplete />} />
       <Route path="/todo-app" element={<TodoApp />} />
+      <Route path='/image-crousel' element={<ImageCrousel/>}/>
     </Routes>
   );
 }
