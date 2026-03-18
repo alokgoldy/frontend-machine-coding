8ce559280b3604ee1e40bcfb46e5344f1a2af9d3
alokgoldy <alokkrgoldy@gmail.com>
2026-03-18 20:01:36 +0530

    feat:added mouseenter

--- react-test/src/pages/ImageCrousel.jsx
+++ react-test/src/pages/ImageCrousel.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
-
+import '../styles/image-crousel.css';
 
 const images = [
     "https://picsum.photos/id/1018/600/300",
@@ -11,35 +11,47 @@ const images = [
 
 function ImageCrousel() {
     const [activeIndex, setActiveIndex] = useState(0);
+    const [isPaused, setIsPaused] = useState(false);
+    const intervalRef = useRef(null)
 
-    const prevSlide = () => {
-        setActiveIndex(prev => prev === 0 ? images.length - 1 : prev - 1);
+
+
+    const handlePrev = () => {
+        setActiveIndex(prev => prev === 0 ? images.length - 1 : prev - 1)
     }
 
-    const nextSlide = () => {
-        setActiveIndex(prev => (prev + 1) % images.length);
+    const handleNext = () => {
+        setActiveIndex(prev => (prev + 1) % images.length)
     }
 
-    return (
-        <div className='crousel-container'>
-            <button className='crousel-btn  prev' onClick={prevSlide}>
-                ❮
-            </button>
-            <div className='crousel-slider'
-            style={{ transform: `translateX(-${activeIndex * 100}%)` }}
-            >
-                {images.map((img, i) => (<img
-                    key={i}
-                    src={img}
-                    alt={`Slide ${i + 1}`}
-                    className={`crousel-slide ${i === activeIndex ? 'active' : ''}`}
-                />))}
-            </div>
-            <button className='crousel-btn  next' onClick={nextSlide}>
-                ❯
-            </button>
+    useEffect(() => {
+        if (!isPaused) {
+            intervalRef.current = setInterval(() => {
+                handleNext();
+            }, 3000)
+        }
+        return () => clearInterval(intervalRef.current)
+    }, [isPaused])
+    return (<div
+        className='crousel'
+        onMouseEnter={() => setIsPaused(true)}
+        onMouseLeave={() => setIsPaused(false)}
+    >
+        <button className='btn prev' onClick={handlePrev}>
+            ❮
+        </button>
+        <div className="slider" style={{ transform: `translateX(-${activeIndex * 100}%)` }}>
+            {images.map((img, i) => (<img
+                key={i}
+                src={img}
+                alt={`Slide ${i + 1}`}
+                className={`slide ${i === activeIndex ? 'active' : ''}`}
+            />))}
         </div>
-    )
+        <button className='btn next' onClick={handleNext}>
+            ❯
+        </button>
+    </div>)
 }
 
 export default ImageCrousel;
