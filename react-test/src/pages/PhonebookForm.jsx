986cf130b0f9a0069a984493ef2d429bc99eb244
alokgoldy <alokkrgoldy@gmail.com>
2026-03-30 20:42:25 +0530

    update: updated InformationTable

--- react-test/src/pages/PhonebookForm.jsx
+++ react-test/src/pages/PhonebookForm.jsx
@@ -5,7 +5,26 @@ function PhoneBookForm() {
 }
 
 function InformationTable({ entries }) {
-
+  return (
+    <table>
+      <thead>
+        <tr>
+          <td>First Name</td>
+          <td>Last Name</td>
+          <td>Phone Number</td>
+        </tr>
+      </thead>
+      <tbody>
+        {entries.map(({ userFirstName, userLastName, userPhone }, idx) => (
+          <tr key={idx}>
+            <td>{userFirstName}</td>
+            <td>{userLastName}</td>
+            <td>{userPhone}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
 }
 
 function PhoneBook() {
