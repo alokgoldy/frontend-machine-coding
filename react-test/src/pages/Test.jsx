50e3b20bfb44a46da0aa4418d7665d37ea9be09e
alokgoldy <alokkrgoldy@gmail.com>
2026-04-14 20:15:18 +0530

    update: updated tic tac toe intest

--- react-test/src/pages/Test.jsx
+++ react-test/src/pages/Test.jsx
@@ -21,6 +21,17 @@ function Board() {
   const [winner, setWinner] = useState('None');
   const [board, setBoard] = useState(() => Array(9).fill(null));
 
+  const matches = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+  ]
+
   const onReset = () => {
 
   }
@@ -44,6 +55,50 @@ function Board() {
         <Square
           value={board[0]}
           onClick={() => onClick(0)}
+          winner={winner}
+        />
+        <Square
+          value={board[1]}
+          onClick={() => onClick(1)}
+          winner={winner}
+        />
+        <Square
+          value={board[2]}
+          onClick={() => onClick(2)}
+          winner={winner}
+        />
+      </div>
+      <div className='board-row'>
+        <Square
+          value={board[3]}
+          onClick={() => onClick(3)}
+          winner={winner}
+        />
+        <Square
+          value={board[4]}
+          onClick={() => onClick(4)}
+          winner={winner}
+        />
+        <Square
+          value={board[5]}
+          onClick={() => onClick(5)}
+          winner={winner}
+        />
+      </div>
+      <div className='board-row'>
+        <Square
+          value={board[6]}
+          onClick={() => onClick(6)}
+          winner={winner}
+        />
+        <Square
+          value={board[7]}
+          onClick={() => onClick(7)}
+          winner={winner}
+        />
+        <Square
+          value={board[8]}
+          onClick={() => onClick(8)}
           winner={winner}
         />
       </div>
