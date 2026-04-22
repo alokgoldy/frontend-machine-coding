fe0f5380be47092ba40cf3d055cd01f1514fb1b2
alokgoldy <alokkrgoldy@gmail.com>
2026-04-22 22:36:26 +0530

    feat: added test

--- react-test/src/pages/Test.jsx
+++ react-test/src/pages/Test.jsx
@@ -1,138 +1,44 @@
 import { useState } from 'react';
 import '../styles/test.css';
 
+function Square() {
+  return (<div>
 
-function Square({ winner, onClick, value }) {
-
-  const isDisabled = value !== null || winner !== 'None';
-
-  return (<button
-    className='square'
-    onClick={onClick}
-    disabled={isDisabled}
-  >
-    {value}
-  </button>)
+  </div>)
 }
-
 
 function Board() {
   const [player, setPlayer] = useState('X');
   const [winner, setWinner] = useState('None');
   const [board, setBoard] = useState(() => Array(9).fill(null));
 
-  const matches = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [6, 4, 2]
-  ]
-
-  const checkWinner = (newBoard) => {
-
-    for (let i = 0; i < matches.length; i++) {
-      const [a, b, c] = matches[i];
-
-      if (newBoard[a] === player && newBoard[b] === player && newBoard[c] === player) {
-        return true
-      }
-    }
-    return false
-  }
-  const onReset = () => {
-    setBoard(() => Array(9).fill(null));
-    setPlayer('X');
-    setWinner('None');
-  }
-
   const onClick = (index) => {
     const newBoard = [...board];
     newBoard[index] = player;
-    setBoard(newBoard);
-    if (checkWinner(newBoard)) {
-      setWinner(player);
-    } else {
-      setPlayer(player === 'X' ? 'O' : 'X');
-    }
   }
-
-  return (<div className="game-board">
-    <div id='status-area' className='status game-status'>
-      Next Player: <span>{player === 'X' ? 'O' : 'X'}</span>
-    </div>
-    <div id='winner-area' className='winner game-status'>
-      Winner: {winner}
-    </div>
-    <button className='game-reset-btn' onClick={onReset}>
-      Reset
-    </button>
-    <div className='board-grid'>
-      <div className='board-row'>
-        <Square
-          value={board[0]}
-          onClick={() => onClick(0)}
-          winner={winner}
-        />
-        <Square
-          value={board[1]}
-          onClick={() => onClick(1)}
-          winner={winner}
-        />
-        <Square
-          value={board[2]}
-          onClick={() => onClick(2)}
-          winner={winner}
-        />
-      </div>
-      <div className='board-row'>
-        <Square
-          value={board[3]}
-          onClick={() => onClick(3)}
-          winner={winner}
-        />
-        <Square
-          value={board[4]}
-          onClick={() => onClick(4)}
-          winner={winner}
-        />
-        <Square
-          value={board[5]}
-          onClick={() => onClick(5)}
-          winner={winner}
-        />
-      </div>
-      <div className='board-row'>
-        <Square
-          value={board[6]}
-          onClick={() => onClick(6)}
-          winner={winner}
-        />
-        <Square
-          value={board[7]}
-          onClick={() => onClick(7)}
-          winner={winner}
-        />
-        <Square
-          value={board[8]}
-          onClick={() => onClick(8)}
-          winner={winner}
-        />
+  return (
+    <div className='game-board'>
+      <div className='board-grid'>
+        <div className='game-row'>
+          <Square
+            value={board[0]}
+            onClick={() => onClick(1)}
+            winner={winner}
+          />
+        </div>
       </div>
     </div>
-  </div>)
+  )
 }
 
-
 function Test() {
-  return (<div className="game">
-    <div className="game-board">
-      <Board />
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board />
+      </div>
     </div>
-  </div>)
+  )
 }
 
 export default Test;
